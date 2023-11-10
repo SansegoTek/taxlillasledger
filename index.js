84d66d1250689228669763a3b18d41adfaa12bc4
SansegoTek <95613969+SansegoTek@users.noreply.github.com>
2023-11-10 15:20:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const contractFunctions = {
   "0x303e6aa4": "convertMultiple", // 8 ** LP **
   "0xf305d719": "addLiquidityETH", // 9. HAS VALUE  ** LP **
   "0x8dbdbe6d": "deposit", // 4  - ** LP **
-  "0x5d444f91": "rechargeCrystal", // 12
+  "0x5d444f91": "rechargeCrystal", // 120xa9059cbb
   "0x5eac6239": "claimRewards", // 568
   "0xfa863736": "startMeditation", // 740
   "0xe30d4440": "claimAirdrop", // 9
@@ -223,7 +223,7 @@ const findDuplicates = () => {
       label: "",
       description: "",
       feeAmt: "",
-    }; // JSON.stringify(line);
+    };
     theKey = JSON.stringify(theKeyObj);
     if (!map.has(theKey)) {
       map.set(theKey, 1);
@@ -250,48 +250,48 @@ counts of transaction types that are included in CSV
 crack (0xb0ea6a1b) - 0/1                        ** Think this needs deeper look, missing costs eg. gold - Update receipt of pet manually in CSV in the same way as for heroes
 rechargeCrystal (0x5d444f91) - 0/2              ** INCLUDE - THERE ARE JEWEL TRANSFER RECORDS
 convertMultiple (0x303e6aa4) - 0/1              ** This is probably the banker CLAIM button - nothing transferred to me. Just put as cost
-createProfile (0xbd3eaa6f) - 0/1                ** obvious
-approve (0x095ea7b3) - 0/80                     ** obvious
-startQuest (0x68ce0b56) - 0/223                 ** obvious
-open (0x690e7c09) - 0/12                        ** obvious
-startQuest (0xc855dea3) - 0/2394                ** obvious
-completeMeditation (0x756fcd69) - 0/125         ** obvious
-startQuestWithData (0xf51333f5) - 0/1148        ** obvious
-stylist (0x6e278cf1) - 0/1                      ** obvious
-startQuest (0x8a2da17b) - 0/485                 ** obvious
-setApprovalForAll (0xa22cb465) - 0/2            ** obvious
+createProfile (0xbd3eaa6f) - 0/1                ** DONE IN TAXHMY
+approve (0x095ea7b3) - 0/80                     ** DONE IN TAXHMY
+startQuest (0x68ce0b56) - 0/223                 ** DONE IN TAXHMY
+open (0x690e7c09) - 0/12                        ** look into (part of summoning)
+startQuest (0xc855dea3) - 0/2394                ** DONE IN TAXHMY
+completeMeditation (0x756fcd69) - 0/125         ** look into
+startQuestWithData (0xf51333f5) - 0/1148        ** DONE IN TAXHMY
+stylist (0x6e278cf1) - 0/1                      ** DONE IN TAXHMY
+startQuest (0x8a2da17b) - 0/485                 ** DONE IN TAXHMY
+setApprovalForAll (0xa22cb465) - 0/2            ** DONE IN TAXHMY
 
 
 -- Some (find and work out why)
-nativeTransfer (0x) - 9/10                      ** Zero value tx, just record fee
+nativeTransfer (0x) - 9/10                      ** DONE IN TAXHMY
 claimRewards (0x5eac6239) - 74/86               ** No transfers, so just record fee
 deposit (0x8dbdbe6d) - 2/3                      ** No transfers, so just record fee
-completeQuest (0x528be0a9) - 3411/4332          ** Those with no rewards (just need fee record?)
+completeQuest (0x528be0a9) - 3411/4332          ** DONE IN TAXHMY
 startMeditation (0xfa863736) - 105/107          ** No transfers, so just record fee
-cancelQuest (0xfe90ff7d) - 12/30                ** Those with no rewards  (just need fee record?)
+cancelQuest (0xfe90ff7d) - 12/30                ** DONE IN TAXHMY
 swapExactTokensForTokens (0x38ed1739) - 3/5     ** Missing 2 have no transfer records, so just add fee
 
 
 -- All (spot checks)
-swapExactETHForTokens (0x7ff36ab5) - 6/6        ** Look fine
-enter (0xa59f3e0c) - 123/123                    ** Look fine
-leave (0x67dfd4c9) - 29/29                      ** Look fine
-bid (0x598647f8) - 7/7
-addLiquidityETH (0xf305d719) - 2/2
-summonCrystal (0x4ea8a311) - 8/8
-swapExactTokensForETH (0x18cbafe5) - 4/4
-claimAirdrop (0xe30d4440) - 9/9
-addLiquidity (0xe8e33700) - 2/2
-withdraw (0x0ad58d2f) - 1/1
-removeLiquidityETH (0x02751cec) - 1/1
-removeLiquidity (0xbaa2abde) - 1/1
-swapAndRedeem (0x4a517a55) - 5/5
-deposit (0x90d25074) - 3/3
-sellItem (0x096c5e1a) - 17/17
-buyItem (0x9f37092a) - 1/1
-consumeItem (0x106f46e4) - 1/1
-petIncubate (0x4c7f588f) - 1/1
-itemBridge (0xad660825) - 29/29
+swapExactETHForTokens (0x7ff36ab5) - 6/6        ** DONE IN TAXHMY
+enter (0xa59f3e0c) - 123/123                    ** DONE IN TAXHMY
+leave (0x67dfd4c9) - 29/29                      ** DONE IN TAXHMY
+bid (0x598647f8) - 7/7                          ** DONE IN TAXHMY
+addLiquidityETH (0xf305d719) - 2/2              ** DOESN'T LOOK RIGHT, NO RECEIPT OF LP TOKEN
+summonCrystal (0x4ea8a311) - 8/8                ** NO RECEIPT OF NFT
+swapExactTokensForETH (0x18cbafe5) - 4/4        ** DONE IN TAXHMY
+claimAirdrop (0xe30d4440) - 9/9                 ** Look fine
+addLiquidity (0xe8e33700) - 2/2                 ** NO RECEIPT OF TOKEN?
+withdraw (0x0ad58d2f) - 1/1                     ** CHECK
+removeLiquidityETH (0x02751cec) - 1/1           ** Look fine, LABEL?
+removeLiquidity (0xbaa2abde) - 1/1              ** Look fine, LABEL?
+swapAndRedeem (0x4a517a55) - 5/5                ** CHECK - BRIDGING JEWEL
+deposit (0x90d25074) - 3/3                      ** NOTHING RECEIVED
+sellItem (0x096c5e1a) - 17/17                   ** Look fine, LABEL?
+buyItem (0x9f37092a) - 1/1                      ** Look fine, LABEL?
+consumeItem (0x106f46e4) - 1/1                  ** Look fine, LABEL?
+petIncubate (0x4c7f588f) - 1/1                  ** AS ABOVE
+itemBridge (0xad660825) - 29/29                 
 sendHero (0x1efedbe5) - 15/15
 petBridge (0xbb5e613b) - 1/1
 
@@ -304,6 +304,7 @@ ALL SWAPS ARE ALMOST DEFINITELY MISCATEGORISED (although actually, logged as des
   - hmm. heroes received e.g. - 0x270d21ceaf75596acefcc96dda863bab16c070eae4479c448088fcdf4be72e5c
   
 Find the 14 duplicates
+ - 6 of them are same transaction with 2 different fees - e.g 0x58983efd693ad33b2c1862cd64bbaba9d18f55853e4d732d96619d2c6cd5bf3c
 
 Might need to set fees back to ONE so that balances add up
 
